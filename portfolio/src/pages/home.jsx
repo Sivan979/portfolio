ce41413822a909656fb8ef835bba07c5aaf57771
sivan <sivanhannan7@gmail.com>
2025-04-14 17:13:29 +0200

    Build home page layout

--- portfolio/src/pages/home.jsx
+++ portfolio/src/pages/home.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
-import Header from '../components/header'
+import React from 'react';
+import Header from '../components/header';
+import Styles from '../styles/home.module.css';
+
 
 const home = () => {
   return (
-    <>
+    <div className={Styles.container}>
       <Header />
-    </>
+      <div className={Styles.homeContainer}>
+        <div className={Styles.leftSection}>
+          <img src="" alt="profile image" />
+        </div>
+
+        <div className={Styles.rightSection}>
+          <h1>Sivan Hannan</h1>
+          <p className={Styles.intro}>
+            hi my name is sivan hannan and i am 21 years old male.
+            i do software developer at Deltion College in Zwolle and what i like to work with
+            the most is JavaScript with React or React Native, this was a short intro about me.
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
