4fe3c3960c64c7e3392d32ab828f20d36de2d636
sivan <sivanhannan7@gmail.com>
2025-04-16 10:37:32 +0200

    portfolio page update (dandido)

--- portfolio/src/pages/portfolio.jsx
+++ portfolio/src/pages/portfolio.jsx
@@ -1,22 +1,35 @@
 import React from 'react'
 import Header from '../components/header';
 import Styles from '../styles/portfolio.module.css';
+import DandidoImg1 from '../assets/images/dandido/dandido1.png';
+import DandidoImg2 from '../assets/images/dandido/dandido2.png';
+import DandidoImg3 from '../assets/images/dandido/dandido3.png';
+import DandidoImg4 from '../assets/images/dandido/dandido4.png';
 
 
 const projects = [
   {
     title: "DanDido",
-    description: "A shoe app where you get only the discounted shoes",
+    images: [DandidoImg1, DandidoImg2, DandidoImg3, DandidoImg4],
+    description: "A shoe app where you can shop for various brands of shoes in one place. it was developed using HTML, Javascript and CSS.",
     link: "https://sivan979.github.io/Dandido/"
   },
   {
     title: "Memory Game",
+    images: [DandidoImg1, DandidoImg2, DandidoImg3, DandidoImg4],
     description: "A fun memory card game built with HTML, CSS, and JavaScript.",
     link: "https://github.com/@sivan979/memory-game"
   },
   {
     title: "Stars",
-    description: "A React-based starssss with local storage.",
+    images: [DandidoImg1, DandidoImg2, DandidoImg3, DandidoImg4],
+    description: "A movies app used with API developed with React Native.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaadddddddddddddddd",
+    link: "https://Sivan979.github.io/starss/"
+  },
+  {
+    title: "Touchdown Game",
+    images: [DandidoImg1, DandidoImg2, DandidoImg3, DandidoImg4],
+    description: "a game where you can double or triple your money!",
     link: "https://Sivan979.github.io/starss/"
   }
 ];
@@ -26,13 +39,18 @@ const portfolio = () => {
     <div className={Styles.container}>
       <Header />
 
-      <h2>my projects</h2>
-      <div>
+      <p className={Styles.topDescription}>my projects so far</p>
+      <div className={Styles.productContainer}>
         {projects.map( (project, index) => (
-          <div>
-            <p>{project.title}</p>
-            <p>{project.description}</p>
-            <a href={project.link} target="_blank" rel="noopener noreferrer"> view project</a>
+          <div className={Styles.product}>
+            <p className={Styles.title}>{project.title}</p>
+            <div className={Styles.imgContainer}>
+              {project.images.map((img, index) =>(
+                <img key={index} className={Styles.img} src={img} alt="product image" />
+              ))}
+            </div>
+            <p className={Styles.description}>{project.description}</p>
+            <a className={Styles.link} href={project.link} target="_blank" rel="noopener noreferrer"> view project</a>
           </div>
         ))}
       </div>
