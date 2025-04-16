7361abbdf065fb5b101e27a802e297933363210e
sivan <sivanhannan7@gmail.com>
2025-04-16 11:04:38 +0200

    portfolio page update (touchdown)

--- portfolio/src/pages/portfolio.jsx
+++ portfolio/src/pages/portfolio.jsx
@@ -6,6 +6,12 @@ import DandidoImg2 from '../assets/images/dandido/dandido2.png';
 import DandidoImg3 from '../assets/images/dandido/dandido3.png';
 import DandidoImg4 from '../assets/images/dandido/dandido4.png';
 
+import touchdown1 from "../assets/images/touchdown/touchdown1.png";
+import touchdown2 from "../assets/images/touchdown/touchdown2.png";
+import touchdown3 from "../assets/images/touchdown/touchdown3.png";
+import touchdown4 from "../assets/images/touchdown/touchdown4.png";
+
+
 
 const projects = [
   {
@@ -14,23 +20,24 @@ const projects = [
     description: "A shoe app where you can shop for various brands of shoes in one place. it was developed using HTML, Javascript and CSS.",
     link: "https://sivan979.github.io/Dandido/"
   },
+  ,
+  {
+    title: "Stars",
+    images: [DandidoImg1, DandidoImg2, DandidoImg3, DandidoImg4],
+    description: "A movies app used with API developed with React Native.",
+    link: "https://Sivan979.github.io/starss/"
+  },
+  {
+    title: "Touchdown Game",
+    images: [touchdown1,touchdown2,touchdown3,touchdown4],
+    description: "a game where you can double or triple your money!",
+    link: "https://sivan979.github.io/TouchDown-game/"
+  },
   {
     title: "Memory Game",
     images: [DandidoImg1, DandidoImg2, DandidoImg3, DandidoImg4],
     description: "A fun memory card game built with HTML, CSS, and JavaScript.",
     link: "https://github.com/@sivan979/memory-game"
-  },
-  {
-    title: "Stars",
-    images: [DandidoImg1, DandidoImg2, DandidoImg3, DandidoImg4],
-    description: "A movies app used with API developed with React Native.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaadddddddddddddddd",
-    link: "https://Sivan979.github.io/starss/"
-  },
-  {
-    title: "Touchdown Game",
-    images: [DandidoImg1, DandidoImg2, DandidoImg3, DandidoImg4],
-    description: "a game where you can double or triple your money!",
-    link: "https://Sivan979.github.io/starss/"
   }
 ];
 
