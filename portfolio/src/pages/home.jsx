a8d150d88d40f4a11b8ffec61ed1c55bd1700024
sivan <sivanhannan7@gmail.com>
2025-05-07 14:20:13 +0200

    added icons to homepage
    icons added + changed the text

--- portfolio/src/pages/home.jsx
+++ portfolio/src/pages/home.jsx
@@ -1,26 +1,45 @@
 import React from 'react';
 import Header from '../components/header';
 import Styles from '../styles/home.module.css';
+import profileImg from '../assets/profileimage.webp';
+import css from '../assets/css.png';
+import html from '../assets/html.png';
+import javascript from '../assets/java-script.png';
+import react from '../assets/react.png';
+import reactNative from '../assets/react-native.png';
 
 
 const home = () => {
   return (
     <div className={Styles.container}>
+
       <Header />
+
       <div className={Styles.homeContainer}>
         <div className={Styles.leftSection}>
-          <img src="" alt="profile image" />
+          <img className={Styles.profileImg} src={profileImg} alt="profile image" />
         </div>
 
         <div className={Styles.rightSection}>
           <h1>Sivan Hannan</h1>
           <p className={Styles.intro}>
-            hi my name is sivan hannan and i am 21 years old male.
-            i do software developer at Deltion College in Zwolle and what i like to work with
-            the most is JavaScript with React or React Native, this was a short intro about me.
+            Hoi, welkom op mijn pagina! Ik ben Sivan Hannan 21 jaar oud,
+            een enthousiaste en gedreven student Software Developer aan het Deltion College in Zwolle.
+            Programmeren vind ik enorm leuk en interessant,
+            vooral het bouwen van interactieve websites en mobiele apps met HTML, CSS, JavaScript, React en React Native.
+            Mijn doel is om constant te groeien en nieuwe technologieën en tools te ontdekken om mijn vaardigheden verder te ontwikkelen.
           </p>
         </div>
       </div>
+
+      <div className={Styles.iconContainer}>
+        <img className={Styles.icons} src={html} alt="html" />
+        <img className={Styles.icons} src={css} alt="css" />
+        <img className={Styles.icons} src={javascript} alt="javascript" />
+        <img className={Styles.icons} src={react} alt="react" />
+        <img className={Styles.icons} src={reactNative} alt="react native" />
+      </div>
+
     </div>
   )
 }
